7b83256758348f1834e2cca13ff1d59f57ea44ea
Donald <dcrozier@itmgmt.com>
2016-02-14 14:36:02 -0800

    Created HUSD Post Deployment script

--- Testing/library/add_ip_address.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import yaml
-import netaddr
-
-
--- Testing/library/interface_profile.py
+++
@@ -1,7 +0,0 @@
-
-class Interface(object):
-    def __init__(self, ifIndex, ifName):
-        self.ifName = ifName
-        self.ifIndex = ifIndex
-        self.mac_table = []
-        self.flag = 0
